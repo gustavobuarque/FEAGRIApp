18be90d82c18a080839bab8644191283862881d2
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2017-04-05 19:59:32 -0300

    Criado página de contatos, apenas visualmente e está puxando dados de json local.

--- src/pages/contatos/contatos.ts
+++ src/pages/contatos/contatos.ts
@@ -1,22 +1,23 @@
+import { Http } from '@angular/http';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the Contatos page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-contatos',
   templateUrl: 'contatos.html'
 })
 export class ContatosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  contatos: any[];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ContatosPage');
+  //constructor(public navCtrl: NavController, public navParams: NavParams, private connSrv: ConnectionService) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+    this.http.get('data/contatos.json')
+      .map(res => res.json())
+      .subscribe(data => {
+            this.contatos = data;
+      });
   }
 
 }
