fff21eacce6f54df6c5f18687a9a78770344cf13
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2017-04-07 14:15:07 -0300

    Criação da grade horária. Modificação do gitignore para que a pasta data seja possível carregar com os mockups JSON

--- src/pages/grade-horaria/grade-horaria.ts
+++ src/pages/grade-horaria/grade-horaria.ts
@@ -1,5 +1,7 @@
+import { Http } from '@angular/http';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
 /*
   Generated class for the GradeHoraria page.
@@ -13,10 +15,58 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class GradeHorariaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  horarios: any[];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GradeHorariaPage');
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
+
+    let hoje = new Date();
+    let semana = '';
+    let diasemana = '';
+    let ano = hoje.getFullYear();
+      switch (new Date().getDay()) {
+          case 0:
+              semana = "0domingo";
+              diasemana = "Domingo"
+              break;
+          case 1:
+              semana = "1segunda";
+              diasemana = "Segunda-feira"
+              break;
+          case 2:
+              semana = "2terça";
+              diasemana = "Terça-feira"
+              break;
+          case 3:
+              semana = "3quarta";
+              diasemana = "Quarta-feira"
+              break;
+          case 4:
+              semana = "4quinta";
+              diasemana = "Quinta-feira"
+              break;
+          case 5:
+              semana = "5sexta";
+              diasemana = "Sexta-feira"
+              
+              break;
+          case 6:
+              semana = "6sábado";
+              diasemana = "Sábado"
+      } // End Switch
+      console.log("Semana " + semana + " Ano " + ano);
+
+      let url = 'data/gradehoraria.json';
+      //let url = "http://www.feagri.unicamp.br/portal/sistemas-intranet/grade-horarios?salaaula_ativa=S&salaaula_ano="+ano+"&salaaula_anosemestre=1&salaaula_semana="+semana;
+
+      this.http.get(url) // Acessa a Url
+        .map(res => res.json()) // Converte o conteúdo da Url para JSON
+        .subscribe(data => { // Passa o objeto JSON para dentro de um array na Classe ContatosPage
+              this.horarios = data;
+        },
+        err => {
+          console.log(err);
+        });
+  
+  } //End Constructor
 
 }
