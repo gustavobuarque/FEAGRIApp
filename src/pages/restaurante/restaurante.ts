24744d04d4bb1489539c22790901ef36c42ba456
gustavobuarque <gustavobuarque@gmail.com>
2017-09-04 12:29:49 -0300

    Adicionado o Cardapio. Faltam ajustes de layout.

--- src/pages/restaurante/restaurante.ts
+++ src/pages/restaurante/restaurante.ts
@@ -1,6 +1,8 @@
+import { Http } from '@angular/http';
 
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
 
 @Component({
@@ -9,19 +11,65 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class RestaurantePage {
 
-  serchQuery: string='';
-
-  
+  items: any[];
+  dias: string[];
+  cardapioHoje: any[];
+   
   constructor(
     public navCtrl: NavController, 
-    public navParams: NavParams
+    public navParams: NavParams,
+    private http: Http
     ){
-      
-    } 
-    
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RestaurantePage');
-  }
+      let url = 'assets/data/cardapio.json';
+      //let url = 'http://www.feagri.unicamp.br/portal/app/cardapio.json';
+  
+      this.http.get(url) // Acessa a Url
+        .map(res => res.json()) // Converte o conteúdo da Url para JSON
+        .subscribe(data => { // Passa o objeto JSON para dentro de um array na Classe ContatosPage
+            this.items = data.CARDAPIO;
+            this.showData(this.dataHoje());
+        },
+        err => {
+          console.log(err);
+        });
+
+    } 
+
+    dataHoje(): string {
+      let hoje = new Date();
+      let meses = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+      let d = '';
+
+      if(hoje.getDate() < 10){
+        d = '0' + hoje.getDate().toString(); 
+      } else {
+        d = hoje.getDate().toString(); 
+      }
+  
+      let m = hoje.getMonth()+1;
+      let y = hoje.getFullYear();
+      let ymd = hoje.getFullYear() + '-' + meses[hoje.getMonth()] + '-' + d;
+
+      return ymd;
+    }
+    
+    diaSemana(): string {
+      let dias = ['Segunda','Terça', 'Quarta', 'Quinta', 'Sexta'];
+      return dias[new Date().getDay()-1];
+    }
+
+    fimSemana(): boolean{
+      if(new Date().getDay() == 0 || new Date().getDay() == 6 ){
+        return true;
+      }
+      return false;
+    }
+
+    showData(selectValue){
+      this.cardapioHoje = this.items.filter((item)=>{
+        return item.DATA == selectValue;
+      });
+    }
 
 }
